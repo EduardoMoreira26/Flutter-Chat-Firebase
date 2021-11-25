e7d8b7b6a9758ee6ddcd869af0cc422996452b8f
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-11-25 10:52:06 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat/src/components/loading_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoadingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}

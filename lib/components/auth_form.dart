85c8c08510dc9b4a6efee71d6580c59e5e97609d
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-11-25 12:14:49 -0300

    create validate form

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -13,7 +13,8 @@ class _AuthFormState extends State<AuthForm> {
   final _formData = AuthFormData();
 
   void _submit() {
-    _formKey.currentState?.validate();
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if(!isValid) return;
   }
 
   @override
@@ -32,12 +33,26 @@ class _AuthFormState extends State<AuthForm> {
                   initialValue: _formData.name,
                   onChanged:(name) =>  _formData.name = name,
                   decoration: InputDecoration(labelText: 'Nome'),
+                  validator: (_name){
+                    final name = _name ?? 'Nome';
+                    if (name.trim().length < 5) {
+                      return 'Nome deve ter no mínimo 5 caracteres.';
+                    }
+                    return null;
+                  },
                 ),
               TextFormField(
                   initialValue: _formData.email,
                   onChanged:(email) =>  _formData.email = email,
                 key: ValueKey('email'),
                 decoration: InputDecoration(labelText: 'E-mail'),
+                 validator: (_email){
+                    final email = _email ?? 'Nome';
+                    if (!email.contains('@')) {
+                      return 'E-mail informado não é válido.';
+                    }
+                    return null;
+                  },
               ),
               TextFormField(
                   initialValue: _formData.password,
@@ -45,6 +60,13 @@ class _AuthFormState extends State<AuthForm> {
                 key: ValueKey('password'),
                 obscureText: true,
                 decoration: InputDecoration(labelText: 'Senha'),
+                validator: (_password){
+                    final password = _password ?? 'Nome';
+                    if (password.length < 6) {
+                      return 'Senha deve ter no mínimo 6 caracteres.';
+                    }
+                    return null;
+                  },
               ),
               SizedBox(height: 12),
               ElevatedButton(

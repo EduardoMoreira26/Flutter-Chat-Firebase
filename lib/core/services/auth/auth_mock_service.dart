142e4b4afb4831e30ee7504c37879e60f272e3fe
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-11-27 16:19:38 -0300

    create auth service and controller

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -28,7 +28,7 @@ class AuthMockService implements AuthService {
       id: Random().nextDouble().toString(),
       name: name,
       email: email,
-      imageUrl: image.path,
+      imageUrl: image.path ?? '/assets/images/...',
     );
 
     _users.putIfAbsent(email, () => newUser);

25d8d266902211e7989347faaa8ccfdc97dca568
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-12-29 13:25:28 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:chat/core/services/notification/push_notification_service.dart';
 import 'package:chat/pages/auth_our_app_page.dart';
 import 'package:chat/pages/auth_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +11,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
